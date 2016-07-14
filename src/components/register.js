501b3a29936f4807780915322b0d3a97eca17f57
avichalp <pandey_avichal@yahoo.com>
2016-07-14 21:56:53 +0530

    refactor register component

--- src/components/register.js
+++ src/components/register.js
@@ -2,58 +2,73 @@ import React from 'react';
 import { Link } from 'react-router';
 import style from '../style';
 
+export default class Registration extends React.Component {
 
-export default React.createClass({
-
-    save: function(key, toPersist) {
-	var paths = JSON.parse(localStorage.getItem('paths') || '{}');
-	paths[key] = toPersist;
-	localStorage.setItem('paths', JSON.stringify(paths));
-    },
-
-    getInitialState: function() {
-	return {
+    constructor(props, context) {
+	super(props, context);
+	this.state = {
 	    url: "api.grofers.com",
 	    dashboards: []
 	}
-    },
+    }
 
-    handleUrlChange: function(evt) {
-	this.setState({url: evt.target.value});
-    },
+    componentDidMount() {
+	const paths = JSON.parse(localStorage.getItem('paths') || '{}');
+	this.setState({
+	    dashboards: Object.keys(paths)
+	});
+    }
 
-    handleNameChange: function(evt) {
-	this.setState({name: evt.target.value})
-    },
+     handleUrlChange(evt) {
+	 this.setState({url: evt.target.value});
+    }
 
-    handleRegister: function() {
+     handleNameChange(evt) {
+	 this.setState({name: evt.target.value})
+    }
+
+     handleRegister() {
 	this.save(this.state.name, {url: this.state.url, columns: []});
 	const paths = JSON.parse(localStorage.getItem('paths') || '{}');
 	this.setState({
 	    dashboards: Object.keys(paths)
 	});
-    },
-
-    componentDidMount: function() {
-	const paths = JSON.parse(localStorage.getItem('paths') || '{}');
-	this.setState({
-	    dashboards: Object.keys(paths)
-	});
-    },
-
-    render: function() {
-	return (
-		<div>
-		<div>
-		<input style={style.input} placeholder="Name" value={this.state.value} onChange={this.handleNameChange}/>
-		<input style={style.input} placeholder="Api Endpoint" value={this.state.value} onChange={this.handleUrlChange}/>
-		<button style={style.button.go} onClick={this.handleRegister}>Register</button>
-		</div>
-		<div>
-		{this.state.dashboards.map(function (p) {return <li key={p}><Link to={'/dashboards/' + p}>{p}</Link></li>})}
-	    </div>
-		</div>
-	);
-
     }
-});
+
+    populateLinks(link) {
+	return (
+	    <li
+	      key={link}>
+	    <Link
+	      to={'/dashboards/' + link}>{link}
+	    </ Link>
+	    </li>
+	);
+    }
+
+    render() {
+	return (
+	    <div>
+	    <div>
+	    <input
+	      style={style.input}
+	      placeholder="Name"
+	      value={this.state.value}
+	      onChange={this.handleNameChange} />
+	    <input
+	      style={style.input}
+	      placeholder="Api Endpoint"
+	      value={this.state.value}
+	      onChange={this.handleUrlChange} />
+	    <button
+	      style={style.button.go}
+	      onClick={this.handleRegister}>Register </button>
+	    </div>
+	    <div>
+	    {this.state.dashboards.map(this.populateLinks)}
+	     </div>
+	    </div>
+	);
+    }
+
+}
